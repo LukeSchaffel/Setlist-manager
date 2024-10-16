f5cd8031c241c6276dd5d1b38c0d4aefc1c3e853
Luke Schaffel <97476326+LukeSchaffel@users.noreply.github.com>
2024-10-16 14:13:26 +0000

    add auth route, file structure enhancements, text input

---
+++ app/(tabs)/auth/index.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { Input } from "@/components";
+
+const AuthScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Create account</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Input label="Username" />
+      <Input label="Password" />
+    </View>
+  );
+};
+
+export default AuthScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
---
+++ components/Input.tsx
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { TextInput, TextInputProps, StyleSheet } from "react-native";
+
+import { View, Text } from "./Themed";
+
+interface InputProps extends TextInputProps {
+  lightColor?: string;
+  darkColor?: string;
+  label?: string;
+}
+
+const Input = (props: InputProps) => {
+  const { style, lightColor, darkColor, label, ...otherProps } = props;
+  const [isFocused, setIsFocused] = useState(false);
+  return (
+    <View style={[styles.wrapper]}>
+      {label && <Text style={[styles.labelText]}>{label}</Text>}
+      <TextInput
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        style={[styles.input, style]}
+        {...otherProps}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: "100%",
+    padding: 16,
+  },
+  input: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    height: 48,
+    minWidth: 32,
+    fontSize: 16,
+    paddingLeft: 16,
+  },
+  labelText: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
+
+export default Input;
